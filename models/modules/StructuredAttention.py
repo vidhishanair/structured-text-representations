40deb0216903cf6872dd1ccc90e920f0fd3fbcbb
Vidhisha Balachandran <vidhishabalachandran@Vidhishas-MacBook-Pro.local>
2018-10-15 22:29:31 -0400

    AS SA

--- models/modules/StructuredAttention.py
+++ models/modules/StructuredAttention.py
@@ -9,6 +9,7 @@ class StructuredAttention(nn.Module):
         super(StructuredAttention, self).__init__()
         self.device = device
         self.sem_dim_size = sem_dim_size
+        self.self.rep_dim = sem_dim_size
         self.str_dim_size = sent_hiddent_size - self.sem_dim_size
         self.tp_linear = nn.Linear(self.str_dim_size, self.str_dim_size, bias=True)
         self.tc_linear = nn.Linear(self.str_dim_size, self.str_dim_size, bias=True)
@@ -87,7 +88,78 @@ class StructuredAttention(nn.Module):
 
         return output
 
+
+    def aditya_code(self, input_var):
+        batch_size, num_words, inp_dim = input_var.size()
+
+        assert inp_dim == self.input_dim, "Dimensions Mismatch Expected %s, Got %s" % (self.input_dim, inp_dim)
+
+        inpe = input_var[:,:,:self.rep_dim]
+        inpd = input_var[:,:,self.rep_dim:]
+
+        tp = F.tanh(self.wplinear(inpd))
+        tc = F.tanh(self.wclinear(inpd))
+
+        tpr = tp.repeat(1,1,num_words).view(batch_size,num_words,num_words,self.att_dim) \
+            .view(batch_size*num_words*num_words,self.att_dim)
+        tcr = tc.repeat(1,num_words,1).view(batch_size,num_words,num_words,self.att_dim) \
+            .view(batch_size*num_words*num_words,self.att_dim)
+
+        fij = self.wabilinear(tpr,tcr).view(batch_size, num_words, num_words)
+
+        fr = torch.exp(self.wrlinear(inpd).squeeze(2))
+
+        Aij = torch.exp(fij)
+        maskij = 1.-torch.eye(num_words).unsqueeze(0).expand(batch_size, num_words, num_words).to(self.device)
+
+        Aij = Aij*maskij
+
+        Di = torch.sum(Aij, dim = 1)
+        Dij = torch.stack([torch.diag(di) for di in Di])
+        Lij = -Aij + Dij
+
+        LLij = Lij[:,1:,:]
+        Lfr = fr.unsqueeze(1)
+        LLxij = torch.cat([Lfr, LLij], dim = 1)
+
+        #Batch Inverse not available in Pytorch
+        LLinv = torch.stack([torch.inverse(li) for li in LLxij])
+
+        d0 = fr * LLinv[:,:,0]
+
+        #Batch Diagonalization not available in pytorch
+        LLinv_diag = torch.stack([torch.diag(lid) for lid in LLinv]).unsqueeze(2)
+
+        tmp1 = (LLinv_diag * Aij.transpose(1,2)).transpose(1,2)
+        tmp2 = Aij * LLinv_diag.transpose(1,2)
+
+        temp11 = torch.zeros(batch_size,num_words,1)
+        temp21 = torch.zeros(batch_size,1,num_words)
+
+        temp12 = torch.ones(batch_size,num_words,num_words-1)
+        temp22 = torch.ones(batch_size,num_words-1,num_words)
+
+        mask1 = torch.cat([temp11,temp12],2).to(self.device)
+        mask2 = torch.cat([temp21,temp22],1).to(self.device)
+
+
+        dx = mask1 * tmp1 - mask2 * tmp2
+
+        d = torch.cat([d0.unsqueeze(1), dx], dim = 1)
+        df = d.transpose(1,2)
+
+        ssr = torch.cat([self.exparam.repeat(batch_size,1,1), inpe], 1)
+        pinp = torch.bmm(df, ssr)
+
+        cinp = torch.bmm(dx, inpe)
+
+        finp = torch.cat([inpe, pinp],dim = 2)
+        output = F.relu(self.fzlinear(finp))
+
+        return output
+
     def b_inv(self, b_mat):
         eye = b_mat.new_ones(b_mat.size(-1)).diag().expand_as(b_mat).to(self.device)
+        eye = torch.eye(b_mat.size(1), b_mat.size(2)).unsqueeze(0).expand(b_mat.size(0), b_mat.size(1), b_mat.size(2)).to(self.device)
         b_inv, _ = torch.gesv(eye, b_mat)
         return b_inv
