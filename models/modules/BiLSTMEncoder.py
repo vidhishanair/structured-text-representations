f554d3931d98b097da4a39b8f2162ac9f05dd982
Vidhisha Balachandran <vidhishabalachandran@Vidhishas-MacBook-Pro.local>
2018-10-20 19:31:32 -0400

    Changing bilstm hidden size

--- models/modules/BiLSTMEncoder.py
+++ models/modules/BiLSTMEncoder.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 class BiLSTMEncoder(nn.Module):
     def __init__(self, hidden_size, input_size, num_layers, dropout=0.5, bidirectional=True):
         super(BiLSTMEncoder, self).__init__()
-        self.bilstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size//2, num_layers=num_layers,
+        self.bilstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers,
                               batch_first=True, bidirectional=bidirectional, dropout=dropout)
 
     def forward(self, input):
