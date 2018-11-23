67d5eb5a7472fdbba05d34bc58b64b4a13e5f288
Vidhisha Balachandran <vidhishabalachandran@Vidhishas-MacBook-Pro.local>
2018-11-23 11:26:14 -0500

    Code refactoring

--- models/DocumentClassificationModel.py
+++ models/DocumentClassificationModel.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from models.modules.BiLSTMEncoder import BiLSTMEncoder
 from models.modules.StructuredAttention import StructuredAttention
-from models.modules.SA2 import StructuredAttention as SA2
 import itertools
 import time
 
@@ -70,48 +69,36 @@ class DocumentClassificationModel(nn.Module):
 
         #BiLSTM
         encoded_sentences, hidden = self.sentence_encoder.forward_packed(input, sent_l)
-#         print("--- %s seconds for BiLSTM ---" % (time.time() - start_time))
-        
+
         mask = tokens_mask.view(tokens_mask.size(0)*tokens_mask.size(1), tokens_mask.size(2)).unsqueeze(2).repeat(1,1,encoded_sentences.size(2))
         encoded_sentences = encoded_sentences * mask
-        #del mask
-        
+
         #Structure ATT
         encoded_sentences, sent_attention_matrix = self.sentence_structure_att.forward(encoded_sentences)
-        #encoded_sentences = self.sentence_structure_att2.forward(encoded_sentences)
-        #print("SA1 : ")
-        #print(encoded_sentences1)
-        #print("SA2: ")
-        #print(encoded_sentences2)
-        #exit()
-#         print("--- %s seconds for Structured Attention for sentence ---" % (time.time() - start_time))
+
         #Reshape and max pool
         encoded_sentences = encoded_sentences.contiguous().view(batch_size, sent_size, token_size, encoded_sentences.size(2))
         encoded_sentences = encoded_sentences + ((tokens_mask-1)*999).unsqueeze(3).repeat(1,1,1,encoded_sentences.size(3))
         encoded_sentences = encoded_sentences.max(dim=2)[0] # Batch * sent * dim
-#         print("--- %s seconds for Reshaping and pooling ---" % (time.time() - start_time))
+
         #Doc BiLSTM
         encoded_documents, hidden = self.document_encoder.forward(encoded_sentences, doc_l)
-#         print("--- %s seconds for BiLSTM for document ---" % (time.time() - start_time))
         mask = sent_mask.unsqueeze(2).repeat(1,1,encoded_documents.size(2))
         encoded_documents = encoded_documents * mask
-        #del mask
-        #structure Att
 
+        #structure Att
         encoded_documents, doc_attention_matrix = self.document_structure_att.forward(encoded_documents)
-#         print("--- %s seconds for Structured Attention document ---" % (time.time() - start_time))
-        
-        #print(encoded_documents.size())
+
         #Max Pool
         encoded_documents = encoded_documents + ((sent_mask-1)*999).unsqueeze(2).repeat(1,1,encoded_documents.size(2))
         encoded_documents = encoded_documents.max(dim=1)[0]
+
         #encoded_documents = self.drop(encoded_documents)
         #encoded_documents = F.relu(self.pre_lin1(encoded_documents))
         #encoded_documents = self.drop(encoded_documents)
         #encoded_documents = F.relu(self.pre_lin2(encoded_documents))
+
         #Linear for output
         output = self.linear_out(encoded_documents)
-        
-#         print("--- %s seconds final ---" % (time.time() - start_time))
-        
+
         return output, sent_attention_matrix, doc_attention_matrix
