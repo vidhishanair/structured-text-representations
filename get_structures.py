34047ebf98559c7f21473e2d5e952052b08688e1
Vidhisha Balachandran <vidhishanair@gmail.com>
2019-03-13 22:46:12 -0400

    computing scores

--- get_structures.py
+++ get_structures.py
@@ -145,7 +145,7 @@ def extract_structures(model, test_batches, device, vocab, dirName):
             #print(doc_attention_matrix.size())
 
             sentence_importance_vector = doc_attention_matrix[:,:,1:].sum(dim=1) * feed_dict['mask_sents']
-            sentence_importance_vector = doc_attention_matrix / doc_attention_matrix.sum(dim=1, keepdim=True).repeat(1, sentence_importance_vector.size(1))
+            sentence_importance_vector = sentence_importance_vector / sentence_importance_vector.sum(dim=1, keepdim=True).repeat(1, sentence_importance_vector.size(1))
             token_level_sentence_scores = sentence_importance_vector.unsqueeze(1).repeat(1, token_size, 1).view(batch_size, sent_size*token_size)
             doc_attention_matrix = doc_attention_matrix[:,:,1:]
             print(token_level_sentence_scores)
